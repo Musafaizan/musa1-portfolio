ade453efb68779ac7d2e81574ff8657c514d533d
Musafaizan <iammusa182@gmail.com>
2026-03-12 15:36:41 +0500

    changes

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -62,8 +62,8 @@ const Loading = ({ percent }: { percent: number }) => {
       <div className="loading-screen">
         <div className="loading-marquee">
           <Marquee>
-            <span> Full Stack Developer</span> <span>Software Engineer</span>
-            <span> Full Stack Developer</span> <span>Software Engineer</span>
+            <span> WELCOME TO MY PORTFOLIO</span> <span>FRONTEND DEVELOPER</span>
+           <span> WELCOME TO MY PORTFOLIO</span> <span>FRONTEND DEVELOPER</span>
           </Marquee>
         </div>
         <div
