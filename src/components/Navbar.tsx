85dbbb181b2ea0121cee96fb11d4161e661c6d58
Musafaizan <iammusa182@gmail.com>
2026-03-16 17:04:17 +0500

    changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -50,7 +50,7 @@ const Navbar = () => {
           className="navbar-connect"
           data-cursor="disable"
         >   
-          iammusa182@gmail.com
+          iammusa182@gmail.com  
         </a>
         <ul>
           <li>
